6cf51d1b6a947d814f0595105e23c2905010036f
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-25 19:41:29 +0500

    feat: initialize database connection and session management using SQLModel

--- Backend/database.py
+++ Backend/database.py
@@ -3,7 +3,11 @@ from config import DATABASE_URL
 
 DATABASE_URL = "postgresql://postgres:123456@localhost:5432/Thambnail_Gen"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False , connect_args={"check_same_thread": False})
 
-def create_db_and_table():
+def create_tables():
     SQLModel.metadata.create_all(engine)    
+
+def get_session():
+    with Session(engine) as session:
+        yield session    
