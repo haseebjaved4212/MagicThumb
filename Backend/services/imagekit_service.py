fa44987c33404fafc297a940a0371e4a90602ec0
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-07-27 08:18:15 +0500

    feat: implement ImageKit and OpenAI service modules for thumbnail generation and cloud storage

--- Backend/services/imagekit_service.py
+++ Backend/services/imagekit_service.py
@@ -11,7 +11,7 @@ def upload_file(file_bytes: bytes , file_name : str , folder : str, content_type
     """
     Uploads a file to ImageKit and returns the CDN URL.
     """
-    result = imagekit.files.upload(file=(file_bytes, "", file_name , content_type),
+    result = imagekit.files.upload(file=(file_bytes, file_name, content_type),
      file_name=file_name,
      folder = folder,
      is_private_file=False,
